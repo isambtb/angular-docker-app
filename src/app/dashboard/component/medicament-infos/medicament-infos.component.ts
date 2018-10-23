005d988b639a540dd0369f6835c7c3561e857df5
issam boutaib <isambtb@gmail.com>
2018-10-24 00:41:42 +0100

    changes

--- src/app/dashboard/component/medicament-infos/medicament-infos.component.ts
+++ src/app/dashboard/component/medicament-infos/medicament-infos.component.ts
@@ -7,8 +7,8 @@ import { DetailsService, MedicinalProduct } from './details.service';
   styleUrls: ['./medicament-infos.component.css']
 })
 export class MedicamentInfosComponent implements OnInit {
-  
-  medicamentName: string ="",
+
+  medicamentName: string ="";
 
   medicinalProduct: MedicinalProduct[];
   constructor(public rest:DetailsService) { }
